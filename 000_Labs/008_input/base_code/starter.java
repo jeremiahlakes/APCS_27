6d068a61c3fd9e03608089ab43d4410a6c10676d
jeremiahlakes <jeremiah.lakes11@gmail.com>
2026-09-14 19:50:27 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -7,8 +7,12 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		string text = sc.nextLine();
+		System.out.println("Jeremiah");
+		System.out.println("15");
+		System.out.println("Janurary");
+		System.out.println("27");
+		System.out.println("2011");
+		System.out.println("A buck fifty is $1.50");
 	}
 }
