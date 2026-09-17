4e8fdb8517f709f48890d57f068893eedf706d54
jeremiahlakes <jeremiah.lakes11@gmail.com>
2026-09-17 19:01:29 +0000

    update

--- 102_Exercises/09_Restaurant/basecode/starter.java
+++ 102_Exercises/09_Restaurant/basecode/starter.java
@@ -7,7 +7,14 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// Your code goes below here
-		
+		System.out.println("Hello welcome to the pizza planet!");
+		System.out.println("What is your name?");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("Nice to meet you, " + name + "!");
+		System.out.println("We serve pizza, pasta, and salad. What would you like to  order?");
+		String order = sc.nextLine();
+ 		System.out.println("You ordered: " + order);
+		System.out.println("Thank you for your order, " + name + "! Your " + order + " will be ready shortly.");
 	}
 }
